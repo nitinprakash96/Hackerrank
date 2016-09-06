2373200ccd07edb1a305dbc458ecfb2b1363668c
nitinprakash96 <prakash.nitin63@gmail.com>
2016-09-06 15:01:13 +0530

    game of thrones.cpp

---
+++ Algorithms/Strings/game of thrones.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int cnt[26];
+int main() {
+   
+	string str;
+	cin >> str;
+	for (int i = 0; i < (int)str.size(); i++)
+		cnt[str[i] - 'a']++;
+	int even = 0, odd = 0;
+	for (int i = 0; i < 26; i++)
+	{
+		if (cnt[i] % 2 == 0) even++;
+		else odd++;
+	}
+
+	if (odd <= 1)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+}
+
